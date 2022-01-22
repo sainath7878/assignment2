99570979a00cd35567d59b89c6a9de881ba8ddf7
Sainath <svmsainath@ymail.com>
2022-01-22 23:13:50 +0530

    Feat: added dialog, tabs, drawer

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const btnSnackbar = document.querySelector(".btn-snackbar");
 const snackbarDiv = document.querySelector(".snackbar");
+const tabPill = document.querySelectorAll(".tab-pill");
 
 btnSnackbar.addEventListener("click", function(){
     console.log("clicked");
     snackbarDiv.classList.add("snackbar-transition");
     snackbarDiv
 });
+
